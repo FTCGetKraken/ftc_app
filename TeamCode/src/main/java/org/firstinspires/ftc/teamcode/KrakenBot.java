e62887a6ba92f3524c4d87cb31d5d01b98256a50
Eric He <eric8he@gmail.com>
2017-10-28 14:46:30 -0700

    Finished KrakenBot object

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KrakenBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KrakenBot.java
@@ -25,6 +25,39 @@ public class KrakenBot {
     public static final double INV_CLAW_HOME=0.8;
     public static final double WRIST_HOME=0.3;
 
+    // Local OpMode members
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
+
+    // Constructor
+    public KrakenBot() {}
+
+    // Initialize hardware
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        left_drive  = hwMap.get(DcMotor.class, "left_drive");
+        right_drive = hwMap.get(DcMotor.class, "right_drive");
+        left_drive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        left_drive.setPower(0);
+        right_drive.setPower(0);
+        lower_arm.setPower(0);
+        upper_arm.setPower(0);
+
+        // Set drive motors to run with encoders
+        left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        wrist = hwMap.get(Servo.class, "wrist");
+        left_claw = hwMap.get(Servo.class, "left_claw");
+        right_claw = hwMap.get(Servo.class, "right_claw");
+        wrist.setPosition(WRIST_HOME);
+        left_claw.setPosition(CLAW_HOME);
+        right_claw.setPosition(INV_CLAW_HOME);
+    }
 }
